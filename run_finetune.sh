fa18f49bd0d006e0ae510e5039810c3542279e5b
Neil Mallinar <nmallina@exp-3-58.eth.cluster>
2021-11-29 00:56:48 -0800

    bugfixing run finetune

--- run_finetune.sh
+++ run_finetune.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BASE_MODELS_DIR=/expanse/lustre/projects/csd697/nmallina/bootstrap/models/d9f09x6c
+BASE_MODELS_DIR=/expanse/lustre/projects/csd697/nmallina/bootstrap/models/aug2-cifar5m-base-models/d9f09x6c
 mkdir -p local_logs
 RUN_ID_TAG="n=5000, aug=2, iid=False"
-NUM_CONCCURENT=2
+NUM_CONCURRENT=4
 
 idx=0
 for d in ${BASE_MODELS_DIR}/*/ ; do
@@ -11,7 +11,7 @@ for d in ${BASE_MODELS_DIR}/*/ ; do
   # dir=$(dirname -- "$d")
   # echo "$d"
   # echo "$base $dir"
-  echo "$base, ${RUN_ID_TAG}"
+  echo "Launching $base, ${RUN_ID_TAG}"
   echo "$d/model.pt"
   python -m inftrain.finetune \
          --proj cf100-finetune-nov28 \
@@ -21,11 +21,13 @@ for d in ${BASE_MODELS_DIR}/*/ ; do
          --k 64 \
          --pretrained $d/model.pt \
          --aug 4 \
-         --lr 1e-3 \
-         --epochs 30 \
+         --lr 2e-5 \
+         --epochs 10 \
+         --fast \
          --opt adamw >local_logs/$base.log 2>&1 &
   idx=$((idx+1))
-  if $(($idx%$NUM_CONCCURENT)) ; do
+  if ((  $idx % $NUM_CONCURRENT == 0  )); then
+    echo "waiting to complete.."
     wait
-  done
+  fi
 done
