67cc7f479566d38d31b8de34e57e9ff54e80d2de
bellecarrell <belle.carrell@gmail.com>
2021-11-30 23:15:01 +0000

    Here

--- common/datasets/__init__.py
+++ common/datasets/__init__.py
@@ -11,6 +11,7 @@ from os import listdir
 import pathlib
 import json
 import h5py
+import augmentations
 
 def dload(gpath, localdir='~/tmp/data/', crc=True, overwrite=False):
     ''' Downloads object from GCS into localdir (if not exists), and returns the local filename'''
@@ -66,23 +67,53 @@ class TransformingTensorDataset(Dataset):
 class AugMixDataset(torch.utils.data.Dataset):
   """Dataset wrapper to perform AugMix augmentation."""
 
-  def __init__(self, X, Y, preprocess, no_jsd=False):
+  def __init__(self, X, Y, preprocess, args):
     self.X = X
     self.Y = Y
     self.preprocess = preprocess
-    self.no_jsd = no_jsd
+    self.no_jsd = args.no_jsd
+    self.args = args
 
   def __getitem__(self, i):
     x, y = self.X[i], self.Y[i]
     if self.no_jsd:
-      return aug(x, self.preprocess), y
+      return aug(x, self.preprocess, self.args), y
     else:
-      im_tuple = (self.preprocess(x), aug(x, self.preprocess),
-                  aug(x, self.preprocess))
+      im_tuple = (self.preprocess(x), aug(x, self.preprocess, self.args),
+                  aug(x, self.preprocess, self.args))
       return im_tuple, y
 
   def __len__(self):
     return len(self.X)
+
+def aug(image, preprocess, args):
+  """Perform AugMix augmentations and compute mixture.
+  Args:
+    image: PIL.Image input image
+    preprocess: Preprocessing function which should return a torch tensor.
+  Returns:
+    mixed: Augmented and mixed image.
+  """
+  aug_list = augmentations.augmentations
+  if args.all_ops:
+    aug_list = augmentations.augmentations_all
+
+  ws = np.float32(np.random.dirichlet([1] * args.mixture_width))
+  m = np.float32(np.random.beta(1, 1))
+
+  mix = torch.zeros_like(preprocess(image))
+  for i in range(args.mixture_width):
+    image_aug = image.copy()
+    depth = args.mixture_depth if args.mixture_depth > 0 else np.random.randint(
+        1, 4)
+    for _ in range(depth):
+      op = np.random.choice(aug_list)
+      image_aug = op(image_aug, args.aug_severity)
+    # Preprocessing commutes since all coefficients are convex
+    mix += ws[i] * preprocess(image_aug)
+
+  mixed = (1 - m) * preprocess(image) + m * mix
+  return mixed
 
 def load_pacs(test_percent=0.2):
     domains = ['art_painting', 'cartoon', 'photo', 'sketch']
