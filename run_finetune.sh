d45f1b9acfa4d54bd47cd1d8754f20bd449bda31
Neil Mallinar <nmallinar@pryoninc.com>
2021-12-10 17:11:17 -0800

    add nshots to run_finetune.sh

--- run_finetune.sh
+++ run_finetune.sh
@@ -4,7 +4,7 @@ BASE_PROJ=aug2-cifar5m-base-models
 BASE_RUN_ID=d9f09x6c
 BASE_MODELS_DIR=/expanse/lustre/projects/csd697/nmallina/bootstrap/models/${BASE_PROJ}/${BASE_RUN_ID}
 mkdir -p local_logs
-RUN_ID_TAG="n=5000, aug=2, iid=False"
+RUN_ID_TAG="n=5000, aug=2, iid=False, 20-shot"
 NUM_CONCURRENT=4
 
 TMP_COMPLETED_DIR=/expanse/lustre/projects/csd697/nmallina/bootstrap/tmp_completed/${BASE_PROJ}/${BASE_RUN_ID}
@@ -22,6 +22,7 @@ for d in ${BASE_MODELS_DIR}/*/ ; do
          --proj cf100-finetune-nov28 \
          --wandb_mode online \
          --dataset cifar100 \
+         --nshots 20 \
          --run_id_tag "$base, ${RUN_ID_TAG}" \
          --k 64 \
          --pretrained $d/model.pt \
