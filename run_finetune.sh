613b20474888fcf570a7ee3d324c060ac73c1165
Neil Mallinar <nmallinar@pryoninc.com>
2021-12-14 22:06:12 -0800

    add a skip index to run_finetune

--- run_finetune.sh
+++ run_finetune.sh
@@ -6,37 +6,43 @@ BASE_MODELS_DIR=/expanse/lustre/projects/csd697/nmallina/bootstrap/models/${BASE
 mkdir -p local_logs
 RUN_ID_TAG="n=5000, aug=2, iid=False"
 NUM_CONCURRENT=6
+# 0-indexed so effectively running every 64 saved steps
+NUM_SKIP=63
 
 #TMP_COMPLETED_DIR=/expanse/lustre/projects/csd697/nmallina/bootstrap/tmp_completed/${BASE_PROJ}/${BASE_RUN_ID}
 #mkdir -p $TMP_COMPLETED_DIR
 
+skip_idx=0
 idx=0
 for d in ${BASE_MODELS_DIR}/*/ ; do
-  base=$(basename -- "$d")
-  # dir=$(dirname -- "$d")
-  # echo "$d"
-  # echo "$base $dir"
-  echo "Launching $base, ${RUN_ID_TAG}"
-  echo "$d/model.pt"
-  python -m inftrain.finetune \
-         --proj cf100-25shot-dec10 \
-         --wandb_mode online \
-         --dataset cifar100 \
-         --nshots 25 \
-         --run_id_tag "$base, ${RUN_ID_TAG}" \
-         --k 8 \
-         --pretrained $d/model.pt \
-         --aug 4 \
-         --lr 1e-3 \
-         --epochs 30 \
-         --fast \
-         --opt adamw >local_logs/$base.log 2>&1 &
-  idx=$((idx+1))
-  sleep 10
-  if ((  $idx % $NUM_CONCURRENT == 0  )); then
-    echo "waiting to complete.."
-    wait
+  if $((  $skip_idx % $NUM_SKIP  == 0 )); then
+    base=$(basename -- "$d")
+    # dir=$(dirname -- "$d")
+    # echo "$d"
+    # echo "$base $dir"
+    echo "Launching $base, ${RUN_ID_TAG}"
+    echo "$d/model.pt"
+    python -m inftrain.finetune \
+           --proj cf100-25shot-dec14 \
+           --wandb_mode online \
+           --dataset cifar100 \
+           --nshots 25 \
+           --run_id_tag "$base, ${RUN_ID_TAG}" \
+           --k 8 \
+           --pretrained $d/model.pt \
+           --aug 4 \
+           --lr 1e-3 \
+           --epochs 30 \
+           --fast \
+           --opt adamw >local_logs/$base.log 2>&1 &
+    idx=$((idx+1))
+    sleep 10
+    if ((  $idx % $NUM_CONCURRENT == 0  )); then
+      echo "waiting to complete.."
+      wait
+    fi
   fi
+  skip_idx=$((skip_idx+1))
   #mv $d $TMP_COMPLETED_DIR/$base
 done
 # mv $TMP_COMPLETED_DIR/* $BASE_MODELS_DIR
